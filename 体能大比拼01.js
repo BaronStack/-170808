8c68b85d4fdc7962c98eb4b2da68acdc5fe91f6d
hosts <BaronStack@users.noreply.github.com>
2017-08-08 15:29:39 +0800

    体能训练

---
+++ 体能大比拼01.js
@@ -0,0 +1,28 @@
+/**
+ * Created by MYPRO on 2017/8/8.
+ */
+function isPrime(i) {
+    for (let num = 2;num < Math.sqrt(i); ++i) {
+        if(i % num === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+function decompose(count_num) {
+    let result = '';
+    let i = 2;
+    // let count = count_num;
+    while (i < count_num) {
+        if(count_num % i === 0) {
+            if (isPrime(i) === true) {
+                result += i;
+                count_num = count_num / i;
+            }
+        }else {
+            i++;
+        }
+    }
+    console.log('90='+result+i);
+}
+decompose(90);
