3ba463df90d83217079dc96c16e2555e30063d87
hosts <BaronStack@users.noreply.github.com>
2017-08-09 14:10:24 +0800

    体能大比拼-斐波那契数列

--- 体能大比拼02.js
+++ 体能大比拼02.js
@@ -1,4 +1,14 @@
 /**
  * Created by MYPRO on 2017/8/9.
  */
-console.log('i love you');
+function find_fib(num) {
+    let fib_num  = [];
+    fib_num[0] = 0;
+    fib_num[1] = 1;
+    fib_num[2] = 1;
+    for (let i = 3;i <= 1000; ++i) {
+        fib_num[i] = fib_num[i-1] + fib_num[i-2];
+    }
+    return fib_num[num - 1];
+}
+console.log("斐波纳契数列中第34个数是：" + find_fib(34));
