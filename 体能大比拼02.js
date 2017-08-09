717da001fd408539b789d2e9b392e704e9a04a0e
hosts <BaronStack@users.noreply.github.com>
2017-08-09 14:22:35 +0800

    递归+非递归

--- 体能大比拼02.js
+++ 体能大比拼02.js
@@ -1,7 +1,8 @@
 /**
  * Created by MYPRO on 2017/8/9.
  */
-function find_fib(num) {
+//非递归
+/*function find_fib(num) {
     let fib_num  = [];
     fib_num[0] = 0;
     fib_num[1] = 1;
@@ -10,5 +11,14 @@ function find_fib(num) {
         fib_num[i] = fib_num[i-1] + fib_num[i-2];
     }
     return fib_num[num - 1];
+}*/
+//递归
+function find_fib(num) {
+    if (num === 1)
+        return 0;
+    else if (num === 2 || num === 3)
+        return 1;
+    else
+        return find_fib(num - 1) + find_fib(num - 2);
 }
 console.log("斐波纳契数列中第34个数是：" + find_fib(34));
