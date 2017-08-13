991d49f3db2011eee58c99e05c86dfb755bc9844
hosts <BaronStack@users.noreply.github.com>
2017-08-13 14:07:48 +0800

    FINISHED

---
+++ 体能大比拼06.js
@@ -0,0 +1,8 @@
+/**
+ * 给定一个未排序的整数数组，找到其中位数。
+ */
+function medianNumber(arr) {
+    arr = arr.sort();
+    return (arr.length % 2 === 0) ? (arr[arr.length / 2 - 1] + arr[arr.length / 2]) / 2 : arr[(arr.length - 1) / 2];
+}
+console.log('数组[3,5,9,4,2,7,8,4,3] 的中位数是：'+ medianNumber([3,5,9,4,2,7,8,4,3]));
