92b81c35bb725391f91495837042d8a5b43fb7ea
hosts <BaronStack@users.noreply.github.com>
2017-08-15 14:27:00 +0800

    FINISHED

--- 体能大比拼07.js
+++ 体能大比拼07.js
@@ -1,6 +1,7 @@
 /**
  * Created by MYPRO on 2017/8/15.
  */
+//函数一
 function caculateCount(arr,element) {
     let num = 0;
     arr.map(function (item) {
@@ -9,6 +10,23 @@ function caculateCount(arr,element) {
     });
     return num;
 }
-let arr = [1, 3, 7, 1 ,1,4];
-let element  = 1;
+//函数二
+function caculateCount(arr,element) {
+    return arr.filter(function (item) {
+        if (item === element)
+            return item;
+    }).length;
+}
+//函数三
+function caculateCount(arr,element) {
+    let num = 0;
+    arr.forEach(function (item) {
+        if (item === element)
+            num ++;
+    })
+    return num;
+}
+
+let arr = [1, 3, 7, 1 ,1,4,4,4,4];
+let element  = 4;
 console.log('ARRAY :[1, 3, 7, 1 ,1,4] has element ' + element + ' count is ' + caculateCount(arr,element));
