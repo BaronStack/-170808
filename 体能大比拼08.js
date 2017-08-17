adc37f0c2ceadaea7215eca68c5b71505db02790
hosts <BaronStack@users.noreply.github.com>
2017-08-17 09:32:25 +0800

    others method

--- 体能大比拼08.js
+++ 体能大比拼08.js
@@ -3,19 +3,9 @@
  例如：arr = [‘a’, ‘ab’, ‘ed’, ‘da’, ‘a’, ‘ed’, ‘b’];输出：‘a’, ‘ed’
  */
 function findRepeat(arr) {
-    let array = [];
-    let newArray = [];
-    array = arr.filter (function (item) {
-        return arr.indexOf(item) !== arr.lastIndexOf(item) ;
-    });
-    let num = 0;
-    for (let item of array) {
-        if (num != array.length / 2) {
-            newArray.push(item);
-            num ++;
-        }
-    }
-    return newArray.join();
+    let newArr = arr.filter((elem, index) => arr.lastIndexOf(elem) !== index);
+
+    return [...new Set(newArr)];
 }
-let arr = ['a','ab','ed','da','a','ed','b'];
+let arr = ['a','a','ab','ed','da','a','ed','b'];
 console.log('数组：' + arr + '中的重复元素为：'+findRepeat(arr));
