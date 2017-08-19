4d8e5b136c93f4c65eec4d9961dd839432eae42e
hosts <BaronStack@users.noreply.github.com>
2017-08-19 14:18:30 +0800

    modified answer

--- 体能大比拼11.js
+++ 体能大比拼11.js
@@ -3,16 +3,14 @@
  例如：arr = [‘s’, ‘a’, ‘s’, ‘b’, ‘a’,1,‘1’,1], 输出：[‘s’, ‘a’, ‘b’,1,‘1’]
  */
 function unique(arr) {
-    let array = arr.filter(function (item) {
+    let num = 0;
+    return arr.filter(function (item) {
         if( arr.indexOf(item) === arr.lastIndexOf(item))
             return item;
-    });
-    let num = 0;
-    let array2 = arr.filter(function (item) {
+    }).concat(arr.filter(function (item) {
         if( arr.indexOf(item,num++) !== arr.lastIndexOf(item))
             return item;
-    });
-    return array2.concat(array);
+    }));
 }
 console.log('数组[‘s’, ‘a’, ‘s’, ‘b’, ‘a’,1,‘1’,1] 去重后的结果是 ：');
 console.log(unique(['s', 'a', 's', 'b', 'a',1,'1',1]));
